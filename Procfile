e89ae25f60d3d929d512f628b08b6c6ca18be6f1
Barulev Alexandr <huston.mavr@gmail.com>
2017-11-19 21:29:45 +0300

    fix 2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: "gunicorn films_rating.wsgi"
+web: "gunicorn films_ratings.wsgi"
--- films_ratings/settings.py
+++ films_ratings/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-db_from_env = dj_database_url.config()
+#db_from_env = dj_database_url.config()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,14 +74,14 @@ WSGI_APPLICATION = 'films_ratings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASES['default'].update(db_from_env)
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+#
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
