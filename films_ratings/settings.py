500d71d96e67935315a6fbf0f9d4b496f6f5ebbe
Barulev Alexandr <huston.mavr@gmail.com>
2017-11-25 23:32:19 +0300

    fixed

--- films_ratings/settings.py
+++ films_ratings/settings.py
@@ -74,16 +74,28 @@ WSGI_APPLICATION = 'films_ratings.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'fr',
+            'USER' : 'hust',
+            'PASSWORD' : '951103',
+            'HOST' : '127.0.0.1',
+            'PORT' : '5432',
+        }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fr',
-        'USER' : 'hust',
-        'PASSWORD' : '951103',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
